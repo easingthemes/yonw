3f4884a607bc77686dec338a917ce1203246f164
easingthemes <info@easingthemes.com>
2014-11-17 00:40:44 +0100

    theme gui

--- app/main.js
+++ app/main.js
@@ -1,15 +1,17 @@
 global.$ = $;
 
-//var project_path = require('project_path');
 var abar = require('address_bar');
 var folder_view = require('folder_view');
 var path = require('path');
 var shell = require('nw.gui').Shell;
 var sys = require('sys');
 var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 var fs = require('fs');
 var gui = require('nw.gui');
+//var yez = require('yez');
 
+//app.listen(port);
 
 $(document).ready(function() {
 
@@ -20,13 +22,13 @@ $(document).ready(function() {
     var chooser = $(name);
     chooser.change(function(evt) {
       projectDir = $(this).val();
-        folder.open(projectDir);
-        addressbar.set(projectDir);
-      console.log($(this).val());
-      cdProject = 'cd '+projectDir;
-      //console.log();
-      runCommand(cdProject);
 
+      folder.open(projectDir);
+      addressbar.set(projectDir);
+    
+      cdProject = 'cd '+projectDir;
+      runCommand('cd', projectDir);
+      console.log(projectDir);
     });
 
     chooser.trigger('click');  
@@ -41,6 +43,7 @@ $(document).ready(function() {
     event.preventDefault();
     var newCmd = $('#konzola').val();
     var fullCmd = cdProject +' && '+ newCmd;
+    //var fullCmd = newCmd;
     console.log(fullCmd);
     runCommand(fullCmd);
   });
@@ -62,12 +65,38 @@ $(document).ready(function() {
   
   $('#home').on('click', function(event) {
     event.preventDefault();
-    myCmd = 'tools-v && ruby -v';
+    myCmd = 'node';
     runCommand(myCmd);
   });
 
   function runCommand(name) {
     exec(name,  function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      $('#output').append(stdout +' <br /> '+ stderr);
+       if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+    // var ls  = spawn(name, [arg]);
+
+    // ls.stdout.on('data', function (data) {
+    //   detached: true,
+    //   console.log('stdout: ' + data);
+    // });
+
+    // ls.stderr.on('data', function (data) {
+    //   console.log('stderr: ' + data);
+    // });
+
+    // ls.on('close', function (code) {
+    //   console.log('child process exited with code ' + code);
+    // });
+  };
+
+
+  function openCmd(name) {
+    var comands = spawn(name,  function (error, stdout, stderr) {
       console.log('stdout: ' + stdout);
       console.log('stderr: ' + stderr);
        if (error !== null) {
